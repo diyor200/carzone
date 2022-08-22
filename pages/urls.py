30032f051ba53930f4ec39d0f846d507c22838cb
diyor200 <abdulaxatovdiyorbek40@gmail.com>
2022-08-23 00:13:37 +0500

    creates pages app and configured navigation links

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from pages import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('cars/', views.cars, name='cars'),
+    path('services/', views.services, name='services'),
+    path('contact/', views.contact, name='contact'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+
 def home(request):
     return render(request, 'pages/home.html')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+
+def services(request):
+    return render(request, 'pages/services.html')
+
+
+def cars(request):
+    return render(request, 'pages/cars.html')
+
+
+def contact(request):
+    return render(request, 'pages/contact.html')
