6ced16061e603fb08ed4b26b069f2b0e4384e5c6
diyor200 <abdulaxatovdiyorbek40@gmail.com>
2022-08-23 13:26:35 +0500

    Team model created and customized admin panel

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 
+from .models import Team
+
 
 def home(request):
-    return render(request, 'pages/home.html')
+    teams = Team.objects.all()
+    data = {
+        'teams': teams,
+    }
+    return render(request, 'pages/home.html', data)
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    teams = Team.objects.all()
+    data = {
+        'teams': teams,
+    }
+    return render(request, 'pages/about.html', data)
 
 
 def services(request):
